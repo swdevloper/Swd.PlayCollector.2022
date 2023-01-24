5fdac6de8be020e28b0390a46087a294fe9e54b0
User <User@DEV-VM>
2023-01-24 17:28:35 +0100

    Projektdateien hinzufügen.

---
+++ Swd.PlayCollector.Gui.WpfNet/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Swd.PlayCollector.DataNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Swd.PlayCollector.Gui.WpfNet
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Location loc = new Location();
+            loc.Name = "Testlocation";
+
+            Swd.PlayCollector.DataNet.PlayCollector model = new Swd.PlayCollector.DataNet.PlayCollector();
+            model.Locations.Add(loc);
+            model.SaveChanges();
+
+            List<Location> list = new List<Location>();
+            list = model.Locations.ToList();
+
+
+        }
+    }
+}
