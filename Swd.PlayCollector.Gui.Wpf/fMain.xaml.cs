5f6e908587bb10930f8a333acd7bb222afb3191e
Marcel Butterweck <marcel.butterweck@butterweck.at>
2023-03-02 21:22:16 +0100

    v0.2023.03.02a MainWindow

---
+++ Swd.PlayCollector.Gui.Wpf/fMain.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Swd.PlayCollector.Business;
+using Swd.PlayCollector.Gui.Wpf.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Swd.PlayCollector.Gui.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class fMain : Window
+    {
+        public fMain()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowsViewModel();
+        }
+
+
+
+        private async void btnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            //CollectionItemService service = new CollectionItemService();
+            //this.grdCollectionItems.ItemsSource = await service.GetAllAsync().Result.ToListAsync();
+        }
+    }
+}
